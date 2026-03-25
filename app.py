6e9ab43c4a50d08ba85b0b49bb89fd090e205b18
sanjeev.kumar <sanjeev.kumar12@globallogic.com>
2026-03-25 13:48:30 +0530

    Update Jenkinsfile7

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>Hello, DevOps Engineer!</h1>'
 
+@app.route('/v1/hello')
+def hello_world():
+    return '<h1>Hello, Software Engineer as Deops Engineer!</h1>'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+    app.run(host='0.0.0.0', port=8002)
